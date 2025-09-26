624cae4249769eefcc05051dd362394a19435d21
Cathzer <stcaptainz1309@gmail.com>
2025-09-26 14:25:02 +0700

    idk

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/cathzer-portfolio/',
+  plugins: [react()],
   server: {
     host: true,
     port: 5173, 
